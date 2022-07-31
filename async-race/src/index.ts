2c82a14726829922f9c0201d9e7ce5777cf0a6db
Weranika <veronikalopanova@gmail.com>
2022-07-31 02:13:41 +0200

    init: start async task

---
+++ async-race/src/index.ts
@@ -0,0 +1,5 @@
+import './reset.css';
+
+document.getElementById('root') as HTMLElement;
+
+console.log('hi');
---
+++ async-race/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+    entry: './src/index.ts',
+    mode: 'development',    
+    module: {
+        rules: [
+            { test: /\.txt$/, use: 'raw-loader' },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.ts$/i, 
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(gif|png|jpg|jpeg|svg)?$/,
+                loader: 'url-loader',
+                options: {
+                    name: 'assets/img/[name].[ext]',
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                  // fallback to style-loader in development
+                process.env.NODE_ENV !== 'production'
+                    ? 'style-loader'
+                    : MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
